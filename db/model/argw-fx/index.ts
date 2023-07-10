aefd23d8b078e44c4e5a08bfe4b028099f4d1e7a
lafleurblanche <aquamint@squall.dti.ne.jp>
2023-07-11 01:17:14 +0900

    20230711 新規作成

---
+++ db/model/argw-fx/index.ts
@@ -0,0 +1,4 @@
+export * from './AreaGWFXAdultOnewayTicketRequest';
+export * from './AreaGWFXAdultRoundTripTicketRequest';
+export * from './AreaGWFXHalfOnewayTicketRequest';
+export * from './AreaGWFXHalfRoundTripTicketRequest';
