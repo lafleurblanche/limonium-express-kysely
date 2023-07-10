f2c3e58ca3d0b3c2e41502c11dfa9e9e4755b8d8
lafleurblanche <aquamint@squall.dti.ne.jp>
2023-07-11 01:17:45 +0900

    20230711 新規作成

---
+++ util/argw-fx/index.ts
@@ -0,0 +1,4 @@
+export * from './AreaGWFXTicketRequestOWAFunction';
+export * from './AreaGWFXTicketRequestRTAFunction';
+export * from './AreaGWFXTicketRequestOWHFunction';
+export * from './AreaGWFXTicketRequestRTHFunction';
