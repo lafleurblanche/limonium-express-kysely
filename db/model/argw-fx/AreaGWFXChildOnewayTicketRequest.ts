5735e903026ec91b46c263d6a69868666e5e26a0
lafleurblanche <aquamint@squall.dti.ne.jp>
2023-07-12 23:34:45 +0900

    20230712 新規作成

---
+++ db/model/argw-fx/AreaGWFXChildOnewayTicketRequest.ts
@@ -0,0 +1,22 @@
+/**
+ * ## 発信番号登録情報
+ * * 支線連絡系統
+ * * 片道乗車券(小児運賃)
+ * * FX用
+ * @author lafleurblanche
+ */
+export interface AreaGWFXChildOnewayTicketRequest {
+  id: number
+  req_no: string
+  from_sta: string
+  to_sta: string
+  via_route: string
+  adult_member: string
+  half_member: string
+  child_member: string
+  date_of_use: string
+  total_fare: string
+  base_fare_adult: string
+  base_fare_half: string
+  base_fare_child: string
+}
